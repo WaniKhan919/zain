3ddd425477fd73a3bf5d9fcd47673fc5392210de
wadood <wanikhan919@gmail.com>
2022-11-02 16:39:32 +0500

    landing page done

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Page;
 use App\Models\Service;
 use App\Models\Subscription;
 use Illuminate\Http\Request;
@@ -23,10 +24,30 @@ class SubscriptionController extends Controller
             $model->service_id=$service1->id ?? '';
             $model->click_id=$click_id ?? null;
             if($model->save()){
-                return redirect('/')->with('success','Successs');
+                if($service=='trayef'){
+                    $tel=5646;
+                }else if($service=='haboob'){
+                    $tel=5636;
+                }else if($service=='el3afia'){
+                    $tel=5691;
+                }else if($service=='Ghonia'){
+                    $tel=5635;
+                }else if($service=='ro7'){
+                    $tel=5216;
+                }else if($service=='agmal'){
+                    $tel=5616;
+                }else if($service=='story'){
+                    $tel=5214;
+                }else if($service=='a3lam'){
+                    $tel=5696;
+                }else if($service=='sonan'){
+                    $tel=5699;
+                }
+                $data=Page::first();
+                return view('front.post_back',compact('data','tel'))->with('success','Successs');
             }
         }
-        return redirect('/')->with('error','Failed');;
+        return redirect('/')->with('error','Failed');
     }
 
     public function view()
---
+++ resources/views/admin/page/index.blade.php
@@ -0,0 +1,114 @@
+@extends('admin.layout')
+@section('container')
+
+<div class="pcoded-content">
+  <div class="pcoded-inner-content">
+
+    <!-- Main-body start -->
+    <div class="main-body">
+      <div class="page-wrapper">
+        <!-- Page-header start -->
+        <div class="page-header card">
+          <div class="row align-items-end">
+            <div class="col-lg-8">
+              <div class="page-header-title">
+                <i class="ti-settings bg-c-blue"></i>
+                <div class="d-inline">
+                  <h4>Landnig Page</h4>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                  <li class="breadcrumb-item">
+                    <a href="index.html">
+                      <i class="icofont icofont-home"></i>
+                    </a>
+                  </li>
+                  <li class="breadcrumb-item"><a href="{{ route('admin-dashboard') }}">Dashboard</a>
+                  </li>
+                  <li class="breadcrumb-item"><a href="{{ route('pages.index') }}">Landnig Page</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Page-header end -->
+
+        <!-- Page body start -->
+        <div class="page-body">
+          <div class="row">
+            <div class="col-sm-12">
+              <!-- Basic Form Inputs card start -->
+              <div class="card">
+                <div class="card-block">
+                  <h4 class="sub-title">Landnig Page</h4>
+                  @if (session()->has('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                      {{ session()->get('success') }}
+                    </div>
+                  @endif
+                  @if (session()->has('error'))
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                      {{ session()->get('error') }}
+                    </div>
+                  @endif
+                  <form method="POST" action="{{ route('pages.store') }}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group row" dir="rtl">
+                      <label class="col-sm-2 col-form-label" dir="rtl">
+                        عنوان</label>
+                      <div class="col-sm-10">
+                        <input type="text" dir="rtl" class="form-control" name="title" value="{{ $data->title ?? '' }}">
+                        @error('title')
+                          <span class="text-danger" role="alert">
+                            {{ $message }}
+                          </span>
+                        @enderror
+                      </div>
+                    </div>
+                    <div class="form-group row" dir="rtl">
+                      <label class="col-sm-2 col-form-label" dir="rtl">
+                        وصف</label>
+                      <div class="col-sm-10">
+                        <textarea name="description" idir="rtl" class="form-control">{{ $data->description ?? '' }}</textarea>
+                        @error('description')
+                          <span class="text-danger" role="alert">
+                            {{ $message }}
+                          </span>
+                        @enderror
+                      </div>
+                    </div>
+                    <div class="form-group row" dir="rtl">
+                      <label class="col-sm-2 col-form-label" dir="rtl">زر كتابة</label>
+                      <div class="col-sm-10">
+                        <input type="text" class="form-control" dir="rtl" name="button" value="{{ $data->button ?? '' }}">
+                        @error('button')
+                          <span class="text-danger" role="alert">
+                            {{ $message }}
+                          </span>
+                        @enderror
+                      </div>
+                    </div>
+                    <div class="card-header-right" dir="rtl">
+                      <button class="btn btn-card btn-primary float-right" dir="rtl">تحديث</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Page body end -->
+      </div>
+    </div>
+    <!-- Main-body end -->
+    <div id="styleSelector">
+
+    </div>
+  </div>
+</div>
+
+@endsection
