e4cd955fff4c573eb09395540bcf6c4ce2c63d23
Muzhar Hussain <muzharhussain110@gmail.com>
2022-11-08 13:59:36 +0500

    update subscription controller

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -12,36 +12,48 @@ class SubscriptionController extends Controller
 {
     public function index($service,Request $request)
     {
+
         if (isset($request->msisdn)) {
             $service1=Service::where('postBackUrl', $service)->first();
             $click_id=session()->get('click_id');
-            $model=new Subscription();
-            $model->phone_no=$request->msisdn ?? '';
-            $model->service_name=$service;
-            $model->service_id=$service1->id ?? '';
-            $model->click_id=$click_id ?? null;
-            if ($model->save()) {
-                if (isset($click_id)) {
-                    try {
-                        $postb = 'http://tracking.y2nx.com/postback?cid='. $click_id;
-                        // \Log::channel('postback')->info($postb);
 
-                        $res_post = Http::get($postb);
-                        // \Log::channel('postback')->info($res_post->body() . "\n");
-                        session()->forget('clickid');
+            $model = Subscription::where('phone_no', $request->msisdn)->where('service_id', $service1->id)->where('service_name', $service)->first();
+            if($model){
+                $model->status=1;
+                $model->save();
+            }else{
+                
+                $model = new Subscription();
+                $model->phone_no = $request->msisdn ?? '';
+                $model->service_name = $service;
+                $model->service_id = $service1->id ?? '';
+                $model->click_id = $click_id ?? null;
+                
+                if ($model->save()) {
+                    if (isset($click_id)) {
+                        try {
+                            $postb = 'http://tracking.y2nx.com/postback?cid='. $click_id;
+                            // \Log::channel('postback')->info($postb);
 
-                        return response()->json(['successs'=>'successs','postBack'=>$res_post->body()]);
-                    } catch (\Exception $e) {
-                        return response()->json(['error'=>'error catch']);
+                            $res_post = Http::get($postb);
+                            // \Log::channel('postback')->info($res_post->body() . "\n");
+                            session()->forget('clickid');
+
+                            // return response()->json(['successs'=>'successs','postBack'=>$res_post->body()]);
+                        } catch (\Exception $e) {
+                            return response()->json(['error'=>'error catch']);
+                        }
                     }
+                    // return response()->json(['successs'=>'successs']);
+                } else {
+                    return response()->json(['error'=>'error']);
                 }
-                return response()->json(['successs'=>'successs']);
-            } else {
-                return response()->json(['error'=>'error']);
             }
+
+            return view('front.post_back', ['service' => $service1]);
         }
-        
-        return redirect()->route('front.index');
+
+        return redirect('/');
     }
 
     public function view()
